71a50df59b52e8521718007b224474763c809b0f
valterlbr <72047943+valterlbr@users.noreply.github.com>
2021-04-29 15:42:40 +0100

    29/04 changed

--- XtraVisionRentDvd/src/controller/PaymentController.java
+++ XtraVisionRentDvd/src/controller/PaymentController.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/********************************************/
 package controller;
-//valter lopes de brito2021
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.DefaultListModel;
